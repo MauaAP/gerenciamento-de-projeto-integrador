1948c5f985721613d74bc034f3c2e103acca3537
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-07 18:05:38 -0300

    Add IUserRepository interface for user data management

---
+++ app/shared/domain/interfaces/IUserRepository.ts
@@ -0,0 +1,12 @@
+import { User } from "../../domain/entities/user";
+
+export interface IUserRepository {
+  
+  createUser(user: User): Promise<User>;
+
+  fetchUsers(): Promise<User[]>;
+
+  getUserById(userId: string): Promise<User | null>;
+
+  getUserByEmail(email: string): Promise<User | null>;
+}
