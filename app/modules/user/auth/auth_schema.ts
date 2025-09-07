eadf47343100761d774f13aee86f06fdb4c46f4f
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-07 18:02:52 -0300

    Add auth_schema.ts for defining authentication request and response schemas

---
+++ app/modules/user/auth/auth_schema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const AuthRequest = z.object({
+  email: z.string().email("Endereço de e-mail inválido"),
+  password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
+});
+export type AuthRequest = z.infer<typeof AuthRequest>;
+
+export const AuthResponse = z.object({
+  message: z.string(),
+  token: z.string(),
+  user: z.object({
+    id: z.string(),
+    name: z.string(),
+    email: z.string(),
+    role: z.string(),
+  }),
+});
+export type AuthResponse = z.infer<typeof AuthResponse>;
