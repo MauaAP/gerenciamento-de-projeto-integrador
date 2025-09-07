3530c0e3f6fc95e160a0ed70e5ea775c190c9d26
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-07 18:54:46 -0300

    Refactor Repository class to use dynamoConfig for DynamoDBResources initialization

--- app/shared/repositories/repository.ts
+++ app/shared/repositories/repository.ts
@@ -3,6 +3,7 @@ import { UserRepoMock } from "./mocks/user_repository_mock";
 import { UserRepositoryDynamoDB } from "./database/dynamo/user_repository_dynamo";
 import { Env } from "../../env";
 import type { IUserRepository } from "../domain/interfaces/IUserRepository";
+import { dynamoConfig } from "./database/dynamo/dynamo_config";
 
 export class Repository {
   public userRepo: IUserRepository;
@@ -12,10 +13,7 @@ export class Repository {
     if (Env.STAGE === "test") {
       this.userRepo = new UserRepoMock();
     } else {
-      this.dynamoDb = new DynamoDBResources({
-        tableName: Env.DYNAMO_USER_TABLE,
-        region: Env.AWS_REGION,
-      });
+      this.dynamoDb = new DynamoDBResources(dynamoConfig);
       this.userRepo = new UserRepositoryDynamoDB(this.dynamoDb);
     }
   }
