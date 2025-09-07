afb97e57b34636dcb0fd887187592ce3e4a4de7c
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-07 18:05:47 -0300

    Add JWToken helper class for JWT encoding, decoding, and refreshing

---
+++ app/shared/helpers/jwtoken.ts
@@ -0,0 +1,49 @@
+import jwt from "jsonwebtoken";
+
+export class JWToken {
+
+  static encode(
+    userId: string | object,
+    expDays: number = 1,
+    expHours: number = 0,
+    expMinutes: number = 0,
+    jwtSecret: string = process.env.JWT_SECRET as string
+  ): string {
+    const exp = Math.floor(
+      (Date.now() +
+        expDays * 24 * 60 * 60 * 1000 +
+        expHours * 60 * 60 * 1000 +
+        expMinutes * 60 * 1000) /
+        1000
+    );
+
+    const payload: any = {
+      user_id: userId,
+      exp: exp,
+    };
+    return jwt.sign(payload, jwtSecret, { algorithm: "HS256" });
+  }
+
+  static decode(
+    accessToken: string,
+    jwtSecret: string = process.env.JWT_SECRET as string
+  ): string | object | false {
+    try {
+      const decoded = jwt.verify(accessToken, jwtSecret, {
+        algorithms: ["HS256"],
+      }) as any;
+      return decoded.user_id;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  static refreshToken(
+    refreshToken: string,
+    jwtSecret: string = process.env.JWT_SECRET as string,
+    expDays: number = 1
+  ): string {
+    const userId = JWToken.decode(refreshToken, jwtSecret);
+    return JWToken.encode(userId as string, expDays, 0, 0, jwtSecret);
+  }
+}
