f5cd4a1ce6eb35cb8e09daba2758bc219568c522
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-07 18:35:47 -0300

    Add UserBuilder and RequestCreateUserBuilder classes to user.factory.ts

---
+++ test/unit/factories/user.factory.ts
@@ -0,0 +1,72 @@
+import { User } from "../../app/shared/domain/entities/user";
+import { ROLE } from "../../app/shared/domain/enums/role";
+
+export class UserBuilder {
+  private _userId = "test-id";
+  private _name = "Test User";
+  private _email = "test@example.com";
+  private _role = ROLE.STUDENT;
+  private _password = "TestPassword123";
+
+  userId(userId: string) {
+    this._userId = userId;
+    return this;
+  }
+  name(name: string) {
+    this._name = name;
+    return this;
+  }
+  email(email: string) {
+    this._email = email;
+    return this;
+  }
+  role(role: ROLE) {
+    this._role = role;
+    return this;
+  }
+  password(password: string) {
+    this._password = password;
+    return this;
+  }
+  build(): User {
+    return new User(
+      this._userId,
+      this._name,
+      this._email,
+      this._role,
+      this._password
+    );
+  }
+}
+
+export class RequestCreateUserBuilder {
+  private _name = "Test User";
+  private _email = "test@example.com";
+  private _role = ROLE.STUDENT;
+  private _password = "TestPassword123";
+
+  name(name: string) {
+    this._name = name;
+    return this;
+  }
+  email(email: string) {
+    this._email = email;
+    return this;
+  }
+  role(role: ROLE) {
+    this._role = role;
+    return this;
+  }
+  password(password: string) {
+    this._password = password;
+    return this;
+  }
+  build() {
+    return {
+      name: this._name,
+      email: this._email,
+      role: this._role,
+      password: this._password,
+    };
+  }
+}
