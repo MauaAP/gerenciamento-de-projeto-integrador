9a9a738b72411c8bb073ea8cd7c4567462e4bd1b
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-21 20:25:43 -0300

    feat: implement PartnerRepositoryDynamoDB class with initial method stubs

---
+++ app/shared/repositories/database/dynamo/partner_repository_dynamo.ts
@@ -0,0 +1,29 @@
+import type { IPartnerRepository, PartnerUpdateOptions } from "app/shared/domain/interfaces/IPartnerRepository";
+import type { DynamoDBResources } from "./dynamo_datasource";
+import type { Partner } from "app/shared/domain/entities/partner";
+
+export class PartnerRepositoryDynamoDB implements IPartnerRepository {
+  private db: DynamoDBResources;
+
+  constructor(db: DynamoDBResources) {
+    this.db = db;
+  }
+  createPartner(partner: Partner): Promise<Partner> {
+    throw new Error("Method not implemented.");
+  }
+  fetchPartners(): Promise<Partner[]> {
+    throw new Error("Method not implemented.");
+  }
+  getPartnerById(partnerId: string): Promise<Partner | null> {
+    throw new Error("Method not implemented.");
+  }
+  getPartnerByname(name: string): Promise<Partner | null> {
+    throw new Error("Method not implemented.");
+  }
+  deletePartnerById(partnerId: string): Promise<Partner | null> {
+    throw new Error("Method not implemented.");
+  }
+  updatePartner(partnerId: string, updateOptions: PartnerUpdateOptions): Promise<Partner | null> {
+    throw new Error("Method not implemented.");
+  }
+}
