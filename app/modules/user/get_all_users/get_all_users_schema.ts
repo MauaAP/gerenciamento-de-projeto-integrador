ba825439fb91a39541a8ecd6eb553dd5a1207503
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-22 19:54:17 -0300

    refactor: update userList schema to use object structure in GetAllUsersResponse

--- app/modules/user/get_all_users/get_all_users_schema.ts
+++ app/modules/user/get_all_users/get_all_users_schema.ts
@@ -3,5 +3,11 @@ import { z } from "zod";
 
 export const GetAllUsersResponse = z.object({
     message: z.string(),
-    userList: z.array(z.instanceof(User))
+    userList: z.array(
+        z.object({
+            id: z.string(),
+            name: z.string(),
+            email: z.string(),
+        })
+    )
 })
