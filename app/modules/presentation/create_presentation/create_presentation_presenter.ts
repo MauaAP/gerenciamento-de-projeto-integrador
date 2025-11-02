7bbb8a5d5f3beb31770caeed4c79a75394a3cbc5
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-02 12:22:41 -0300

    fix: update import paths in create_presentation_presenter for consistency

--- app/modules/presentation/create_presentation/create_presentation_presenter.ts
+++ app/modules/presentation/create_presentation/create_presentation_presenter.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
 import { CreatePresentationUseCase } from "./create_presentation_usecase";
 import { CreatePresentationController } from "./create_presentation_controller";
-import { authenticateToken } from "app/shared/middleware/jwt_middleware";
-import { PresentationRepository } from "app/shared/repositories/repository";
+import { authenticateToken } from "../../../shared/middleware/jwt_middleware";
+import { PresentationRepository } from "../../../shared/repositories/repository";
 
 const router= express.Router();
 
