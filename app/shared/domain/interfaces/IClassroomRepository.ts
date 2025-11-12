311386b7e2b8de1e5bd7ef2df644b9559f960f24
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-12 19:22:15 -0300

    feat: add IClassroomRepository interface for classroom management operations

---
+++ app/shared/domain/interfaces/IClassroomRepository.ts
@@ -0,0 +1,8 @@
+import { Classroom } from "../entities/classroom";
+
+export interface IClassroomRepository {
+    createClassroom(classroom: Classroom): Promise<Classroom>;
+    fetchClassrooms(): Promise<Classroom[]>;
+    getClassroomById(classroomId: string): Promise<Classroom | null>;
+}
+
