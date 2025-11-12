43c9a5b7939f5da2b05501f775874ba81c4273fe
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-12 19:22:27 -0300

    feat: add ICourseRepository interface for course management operations

---
+++ app/shared/domain/interfaces/ICourseRepository.ts
@@ -0,0 +1,8 @@
+import { Course } from "../entities/course";
+
+export interface ICourseRepository {
+    createCourse(course: Course): Promise<Course>;
+    fetchCourses(): Promise<Course[]>;
+    getCourseById(courseId: string): Promise<Course | null>;
+}
+
