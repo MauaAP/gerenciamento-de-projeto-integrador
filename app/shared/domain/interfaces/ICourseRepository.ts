0659be91f0aeaa1a7969f9d13f2cb6bee43a6d56
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-13 00:20:45 -0300

    feat: add deleteCourse method to ICourseRepository interface

--- app/shared/domain/interfaces/ICourseRepository.ts
+++ app/shared/domain/interfaces/ICourseRepository.ts
@@ -4,5 +4,6 @@ export interface ICourseRepository {
     createCourse(course: Course): Promise<Course>;
     fetchCourses(): Promise<Course[]>;
     getCourseById(courseId: string): Promise<Course | null>;
+    deleteCourse(courseId: string): Promise<Course | null>;
 }
 
