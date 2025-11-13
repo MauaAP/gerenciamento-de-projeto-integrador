8244ff721f5682af12abed24dfc74b1e6e455707
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-13 00:18:17 -0300

    feat: add DeleteCourseController for handling course deletion requests and responses

---
+++ app/modules/course/delete_course/delete_course_controller.ts
@@ -0,0 +1,41 @@
+import { ForbiddenException } from "../../../shared/helpers/exceptions";
+import { UserFromToken } from "../../../shared/middleware/jwt_middleware";
+import { parseBody } from "../../../shared/utils/parse_body";
+import { Request, Response } from "express";
+import { DeleteCourseRequest, DeleteCourseResponse } from "./delete_course_schema";
+import { DeleteCourseUseCase } from "./delete_course_usecase";
+
+export class DeleteCourseController {
+    constructor(private readonly usecase: DeleteCourseUseCase) {}
+
+    async handler(req: Request, res: Response) {
+        const userFromToken = req.user as UserFromToken;
+
+        const allowedRoles = ["ADMIN", "MODERATOR"]
+                
+        if (!allowedRoles.includes(userFromToken.role)){
+            throw new ForbiddenException(
+                "Você não tem permissão para acessar este recurso"
+            );
+        }
+
+        const {id} = parseBody(
+            DeleteCourseRequest,
+            req.body
+        );
+
+        const course = await this.usecase.execute(id);
+
+        const response = DeleteCourseResponse.parse({
+            message: "Curso deletado com sucesso",
+            course: {
+                id: course.courseId,
+                name: course.name,
+                ...(course.code && { code: course.code })
+            }
+        });
+        
+        res.status(200).json(response)
+    }
+}
+
