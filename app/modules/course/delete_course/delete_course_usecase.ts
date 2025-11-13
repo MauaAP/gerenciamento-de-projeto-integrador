7750b80e1a1ec51ee4ab9b8b9273aa2962ccae84
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-13 00:18:31 -0300

    feat: implement DeleteCourseUseCase for handling course deletion logic

---
+++ app/modules/course/delete_course/delete_course_usecase.ts
@@ -0,0 +1,18 @@
+import { Course } from "../../../shared/domain/entities/course";
+import { ICourseRepository } from "../../../shared/domain/interfaces/ICourseRepository";
+import { NotFoundException } from "../../../shared/helpers/exceptions";
+
+export class DeleteCourseUseCase {
+    constructor(private readonly courseRepository: ICourseRepository) {}
+
+    async execute(courseId: string): Promise<Course> {
+        const course = await this.courseRepository.deleteCourse(courseId);
+
+        if (!course) {
+            throw new NotFoundException("Curso não está no banco");
+        }
+
+        return course;
+    }
+}
+
