00df1d2ec7a8b365e6d79a382817363da6ceefd4
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-13 00:20:06 -0300

    feat: integrate ClassroomRepository and add token authentication to update presentation route

--- app/modules/presentation/update_presentation/update_presentation_presenter.ts
+++ app/modules/presentation/update_presentation/update_presentation_presenter.ts
@@ -1,11 +1,14 @@
 import { PresentationRepository } from "../../../shared/repositories/repository";
+import { ClassroomRepository } from "../../../shared/repositories/repository";
 import express, { Request, Response } from "express";
 import { UpdatePresentationUseCase } from "./update_presentation_usecase";
 import { UpdatePresentationController } from "./update_presentation_controller";
+import { authenticateToken } from "../../../shared/middleware/jwt_middleware";
 
 const router= express.Router();
 
 const repository= new PresentationRepository();
+const classroomRepository = new ClassroomRepository();
 
 const updatePresentationUseCase= new UpdatePresentationUseCase(
     repository.presentationRepo,
@@ -13,12 +16,13 @@ const updatePresentationUseCase= new UpdatePresentationUseCase(
     repository.examinationBoardRepo,
     repository.userRepo,
     repository.projectRepo,
-    repository.partnerRepo
+    repository.partnerRepo,
+    classroomRepository.classroomRepo
 );
 
 const updatePresentationController= new UpdatePresentationController(updatePresentationUseCase);
 
-router.put("/presentation", async (req: Request, res: Response) => {
+router.put("/presentation", authenticateToken, async (req: Request, res: Response) => {
     await updatePresentationController.handler(req, res);
 });
 
