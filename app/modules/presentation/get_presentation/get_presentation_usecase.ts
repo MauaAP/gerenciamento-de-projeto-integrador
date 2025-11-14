96c2830a8bcd8a17e2d127b74257f9d9016dd6b2
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-13 23:25:29 -0300

    refactor: add optional classroomName field to PresentationOficialModel for improved presentation data structure

--- app/modules/presentation/get_presentation/get_presentation_usecase.ts
+++ app/modules/presentation/get_presentation/get_presentation_usecase.ts
@@ -38,6 +38,7 @@ export interface PresentationOficialModel {
     examinationBoard: {
         porfessorNameList: string[];
     };
+    classroomName?: string;
 }
 
 export class GetPresentationUseCase {
