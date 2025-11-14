d012b2e780145fe808ce9f8292e8d16e09c46a33
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-13 23:17:06 -0300

    refactor: update CreateCourseRequest schema to use string validation for course name instead of enum

--- app/modules/course/create_course/create_course_schema.ts
+++ app/modules/course/create_course/create_course_schema.ts
@@ -2,48 +2,7 @@ import { COURSE } from "../../../shared/domain/enums/course";
 import { z } from "zod";
 
 export const CreateCourseRequest = z.object({
-    name: z.enum([
-        "ADMINISTRAÇÃO",
-        "ANÁLISE E DESENVOLVIMENTO DE SISTEMAS",
-        "ARQUITETURA E URBANISMO",
-        "CIÊNCIAS DA COMPUTAÇÃO",
-        "DESIGN",
-        "ECONOMIA",
-        "ENGENHARIA CIVIL",
-        "ENGENHARIA DE ALIMENTOS",
-        "ENGENHARIA DE COMPUTAÇÃO",
-        "ENGENHARIA DE CONTROLE E AUTOMAÇÃO",
-        "ENGENHARIA DE PRODUÇÃO",
-        "ENGENHARIA ELÉTRICA",
-        "ENGENHARIA MECÂNICA",
-        "ENGENHARIA QUÍMICA",
-        "INTELIGÊNCIA ARTIFICIAL E CIÊNCIA DE DADOS",
-        "RELAÇÕES INTERNACIONAIS",
-        "SISTEMAS DE INFORMAÇÃO"
-    ], { 
-        errorMap: () => ({ message: "Course é obrigatório e deve ser um dos valores válidos" }) 
-    }).transform((val) => {
-        const courseMap: Record<string, COURSE> = {
-            "ADMINISTRAÇÃO": COURSE.ADM,
-            "ANÁLISE E DESENVOLVIMENTO DE SISTEMAS": COURSE.ADS,
-            "ARQUITETURA E URBANISMO": COURSE.ARQ,
-            "CIÊNCIAS DA COMPUTAÇÃO": COURSE.CIC,
-            "DESIGN": COURSE.DSN,
-            "ECONOMIA": COURSE.ECO,
-            "ENGENHARIA CIVIL": COURSE.ENCV,
-            "ENGENHARIA DE ALIMENTOS": COURSE.ENAL,
-            "ENGENHARIA DE COMPUTAÇÃO": COURSE.ENCP,
-            "ENGENHARIA DE CONTROLE E AUTOMAÇÃO": COURSE.ENCA,
-            "ENGENHARIA DE PRODUÇÃO": COURSE.ENPD,
-            "ENGENHARIA ELÉTRICA": COURSE.ENEL,
-            "ENGENHARIA MECÂNICA": COURSE.ENMC,
-            "ENGENHARIA QUÍMICA": COURSE.ENQM,
-            "INTELIGÊNCIA ARTIFICIAL E CIÊNCIA DE DADOS": COURSE.IAD,
-            "RELAÇÕES INTERNACIONAIS": COURSE.RIN,
-            "SISTEMAS DE INFORMAÇÃO": COURSE.SIN
-        };
-        return courseMap[val];
-    }),
+    name: z.string({ message: "O nome do curso é obrigatório" }).min(1, { message: "O nome do curso não pode estar vazio" }),
     code: z.string().optional()
 })
 
