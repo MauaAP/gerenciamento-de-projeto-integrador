e94baef62c0864934b32555895256e62b4f6f8ef
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-11-13 23:25:46 -0300

    refactor: remove sala field from UpdatePresentationRequest schema for improved data consistency

--- app/modules/presentation/update_presentation/update_presentation_schema.ts
+++ app/modules/presentation/update_presentation/update_presentation_schema.ts
@@ -10,8 +10,6 @@ export const UpdatePresentationRequest = z.object({
     groupId: z.string({message: "groupId deve ser dado em string"}).length(36, "O group id deve conter 36 caracteres").optional(),
 
     examinationBoardId: z.string({message: "examinationBoardId deve ser dado em string"}).length(36, "O examinationBoard id deve conter 36 caracteres").optional(),
-    
-    sala: z.string({message: "A sala deve ser uma string"}).optional(),
 
     classroomId: z.string({message: "O classroomId deve ser dado em string"}).length(36, "O classroomId deve conter 36 caracteres").optional(),
 
@@ -26,7 +24,7 @@ export const UpdatePresentationRequest = z.object({
         return statusMap[val];
     }).optional()
 }).refine(
-    (data) => (data.date !== undefined || data.groupId !== undefined || data.examinationBoardId !== undefined || data.sala !== undefined || data.classroomId !== undefined || data.status !== undefined),
+    (data) => (data.date !== undefined || data.groupId !== undefined || data.examinationBoardId !== undefined || data.classroomId !== undefined || data.status !== undefined),
     {
         message: "Você deve passar algum atributo para ser alterado"
     }
